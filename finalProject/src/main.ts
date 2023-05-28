1040faa05b18338604bab6923b336681a7c3f8c3
FenrisStargazer <80987915+FenrisStargazer@users.noreply.github.com>
2023-05-28 00:35:28 -0700

    add vuetify???

--- finalProject/src/main.ts
+++ finalProject/src/main.ts
@@ -4,7 +4,35 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 
+// Vuetify
+import 'vuetify/styles'
+import { createVuetify } from 'vuetify'
+import * as components from 'vuetify/components'
+import * as directives from 'vuetify/directives'
+
+const vuetify = createVuetify({
+  components,
+  directives,
+  theme: {
+    defaultTheme: 'dark',
+    themes: {
+      dark: {
+        colors: {
+          primary: '#ff4d00',
+          secondary: '#006600',
+          correct: '#224d00',
+          misplaced: '#db6300',
+          wrong: '#4d0900',
+          unknown: '#222222'
+        }
+      }
+    }
+  }
+})
+
 const app = createApp(App)
+
+app.use(vuetify)
 
 app.use(router)
 
