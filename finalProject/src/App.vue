f9f6e3c773c6e55a9c5995699f77ba066dd56dc7
FenrisStargazer <80987915+FenrisStargazer@users.noreply.github.com>
2023-05-28 00:50:59 -0700

    does this break it?

--- finalProject/src/App.vue
+++ finalProject/src/App.vue
@@ -1,13 +1,25 @@
-<script setup lang="ts">
-import { RouterLink, RouterView } from 'vue-router'
-</script>
-
 <template>
-  
+  <v-app>
+    <span class="bg"></span>
+    <v-app-bar :elevation="3">
+      <template v-slot>
+        <v-app-bar-title>
+          <RouterLink to="/">
+            <v-icon icon="mdi-leaf" color="primary"></v-icon>
+            Wordle
+          </RouterLink>
+        </v-app-bar-title>
+        <v-spacer></v-spacer>
+        <v-btn icon="mdi-trophy-outline"></v-btn>
+      </template>
+    </v-app-bar>
 
-  <RouterView />
+    <v-main>
+      <RouterView />
+    </v-main>
+  </v-app>
 </template>
 
-<style scoped>
-/*  */
-</style>
+<script setup lang="ts">
+//
+</script>
--- finalProject/src/main.ts
+++ finalProject/src/main.ts
@@ -1,18 +1,33 @@
-import './assets/main.css'
-
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
+import { mdi } from 'vuetify/iconsets/mdi'
+
+import './assets/main.css'
 
 // Vuetify
 import 'vuetify/styles'
 import { createVuetify } from 'vuetify'
 import * as components from 'vuetify/components'
 import * as directives from 'vuetify/directives'
+import Axios from 'axios'
+
+//Check if the app is running on localhost
+if (window.location.hostname === 'localhost' || window.location.hostname === '127.0.0.1') {
+  Axios.defaults.baseURL = 'https://localhost:44329/'
+} else {
+  Axios.defaults.baseURL = 'https://abrand3-finalProject.azurewebsites.net/'
+}
 
 const vuetify = createVuetify({
   components,
   directives,
+  icons: {
+    defaultSet: 'mdi',
+    sets: {
+      mdi
+    }
+  },
   theme: {
     defaultTheme: 'dark',
     themes: {
